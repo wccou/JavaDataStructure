4debb76a99ed2c14764358a48097a6d5974dfa07
wccou <1281585844@qq.com>
2018-06-24 21:55:53 +0800

    剑指offer53的三道题

--- Offer/src/me/hitwcc/test/Test53.java
+++ Offer/src/me/hitwcc/test/Test53.java
@@ -53,13 +53,12 @@ public class Test53 {
 
 		int mid=(start+end)/2;
 		while(start<=end){
-			System.out.println(mid);
 			if(array[mid]>k){
 				
 				end=mid-1;
 			}else if(array[mid]<k){
 				start=mid+1;
-			}else if(mid+1<array.length-1&&array[mid+1]==k){
+			}else if(mid+1<=array.length-1&&array[mid+1]==k){
 				start=mid+1;
 			}else{
 				return mid;
@@ -68,11 +67,86 @@ public class Test53 {
 		}
 		return -1;
 	}
+	/**
+	 * 
+	 * @description:0~n-1中缺失的数字 长度为n-1的递增排序数组中的所有数字都是唯一的，都在0~n-1范围内，找出
+	 * 缺失的那个   求和法
+	 * @author: wcc
+	 * @created: 下午9:45:54
+	 * @param number
+	 * @return
+	 */
+	public int findMissingNumber1(int[] number){
+		int length=number.length;
+		int sum=(1+length)*length/2;
+		int sum1=0;
+		for(int i=0;i<length;i++){
+			sum1+=number[i];
+		}
+		return sum-sum1;
+	}
+	/**
+	 * 
+	 * @description:有序数组都要考虑二分查找法
+	 * @author: wcc
+	 * @created: 下午9:47:57
+	 * @param number
+	 * @return
+	 */
+	public int findMissingNumber2(int[] number){
+		int length=number.length;
+		int left=0;
+		int right=length-1;
+		while(left<=right){
+			int mid=(right+left)>>1;
+			if(number[mid]==mid){
+				left=mid+1;
+			}else if(number[mid-1]==mid-1||mid==0){
+				return mid;
+			}else{
+				right=mid-1;
+			}
+		}
+		if(left==length)
+			return length;
+		//System.out.println(left);
+		return -1;
+	}
+	/**
+	 * 
+	 * @description:单调递增数组中数值和下标相等的元素
+	 * @author: wcc
+	 * @created: 下午9:52:23
+	 * @param number
+	 * @return
+	 */
+	public int getNumberSameAsIndex(int[] number){
+		if(number==null||number.length<=0)
+			return -1;
+		int length=number.length;
+		int left=0;
+		int right=length-1;
+		while(left<=right){
+			int mid=(left+right)>>1;
+			if(number[mid]==mid){
+				return mid;
+			}else if(number[mid]<mid){
+				left=mid+1;
+			}else{
+				right=mid-1;
+			}
+		}
+		return -1;
+	}
+	
 	public static void main(String[] args) {
 		Test53 test=new Test53();
 		int[] a=new int[]{3,3,3,3,4,4};
-	
-		int s=test.GetNumberOfK(a, 5);
-		System.out.println(s);
+		int[] b=new int[]{0,1,2,3,4,5};
+		int[] c=new int[]{-3,-1,1,3,5};
+		int s=test.GetNumberOfK(a, 4);
+		int d=test.findMissingNumber1(b);
+		int f=test.getNumberSameAsIndex(c);
+		System.out.println(f);
 	}
 }
