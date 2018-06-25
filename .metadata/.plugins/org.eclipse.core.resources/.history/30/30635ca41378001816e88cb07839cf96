075e5074cc4379830ceaa985b5b058982da04686
wccou <1281585844@qq.com>
2018-06-25 17:20:34 +0800

    剑指offer-54

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/30/30635ca41378001816e88cb07839cf96
@@ -0,0 +1,8 @@
+package me.hitwcc.learn;
+
+
+public class Person{
+    private String name = "Person";
+    int age=0;
+}
+
--- Offer/src/me/hitwcc/learn/Person.java
+++ Offer/src/me/hitwcc/learn/Person.java
@@ -2,7 +2,16 @@ package me.hitwcc.learn;
 
 
 public class Person{
-    private String name = "Person";
-    int age=0;
+	private static int i=1;
+    public int getNext(){
+         return i++;
+    }
+    public static void main(String[] args) {  
+    	String x="fmn";
+    	x.toUpperCase();
+    	String y=x.replace('f','F');
+    	y=y+"wxy";
+    	System.out.println(y);
+     }
 }
 
