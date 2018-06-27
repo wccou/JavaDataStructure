17c979b78d2ff9dcba68820e02ea53181e6a38d0
wccou <1281585844@qq.com>
2018-06-27 10:39:40 +0800

    剑指offer 56-2

--- Offer/src/me/hitwcc/test/Test56.java
+++ Offer/src/me/hitwcc/test/Test56.java
@@ -11,6 +11,15 @@ public class Test56 {
 	public Test56() {
 		// TODO Auto-generated constructor stub
 	}
+	/**
+	 * 
+	 * @description:数组中只出现一次的两个数字
+	 * @author: wcc
+	 * @created: 上午10:14:12
+	 * @param array
+	 * @param num1
+	 * @param num2
+	 */
 	public void FindNumsAppearOnce(int [] array,int num1[] , int num2[]) {
         if(array==null||array.length <2)
         	return;
@@ -41,4 +50,36 @@ public class Test56 {
 		}
 		return index;
 	}
+	/**
+	 * 
+	 * @description:在一个数组中除了一个数字只出现一次外，
+	 * 				其他数字都出现了三次 找出只出现一次的那个数字
+	 * @author: wcc
+	 * @created: 上午10:20:21
+	 * @param array
+	 * @return
+	 */
+	public int FindNumsAppearOnce(int[] array){
+		if(array==null||array.length<=0)
+			return -1;
+		int[] bitTime=new int[32];
+		/*for(int i=0;i<array.length;i++){
+			bitTime[i]=0;
+		}*/
+		int result=0;
+		for(int i=0;i<32;i++){
+			for(int j=0;j<array.length;j++){
+				bitTime[i] +=(array[j]>>i)&1;
+			}
+			result |=(bitTime[i]%3)<<i;
+		}
+		return result;
+	}
+	public static void main(String[] args) {
+		Test56 test=new Test56();
+		int[] a=new int[]{1,1,1,5,3,3,3,4,4,4};
+		int result=test.FindNumsAppearOnce(a);
+		System.out.println(result);
+	}
+	
 }
