f23127f9ba04f9aff7e425e835c8bab42ec7c8c1
wccou <1281585844@qq.com>
2018-06-29 14:49:04 +0800

    剑指offer62 环形链表法

--- Offer/src/me/hitwcc/test/Test62.java
+++ Offer/src/me/hitwcc/test/Test62.java
@@ -36,4 +36,45 @@ public class Test62 {
         }
         return list.size()==1?list.get(0):-1;
     }
+	/**
+	 * 
+	 * @description:环形链表法 先构造环形链表，然后每次删除第m个直到不能再删除
+	 * @author: wcc
+	 * @created: 下午2:46:45
+	 * @param n
+	 * @param m
+	 * @return
+	 */
+	public int LastRemaining_Solution1(int n, int m){
+		if(m<=0||n<=0){
+			return -1;
+		}
+		ListNode head=new ListNode(0);
+		ListNode pre=head;
+		for(int i=1;i<n;i++){
+			ListNode temp=new ListNode(i);
+			pre.next=temp;
+			pre=temp;
+		}
+		pre.next=head;
+		ListNode temp2=null;
+		while(n!=1){
+			temp2=head;
+			for(int i=1;i<m-1;i++){
+				temp2=temp2.next;
+			}
+			temp2.next=temp2.next.next;
+			head=temp2.next;
+			n--;
+		}
+		return head.val;
+	}
+}
+class ListNode{
+	int val;
+	ListNode next;
+	public ListNode(int val) {
+		this.val = val;
+	}
+	
 }
