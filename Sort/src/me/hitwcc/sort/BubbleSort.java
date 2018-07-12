8ae966d72b6770d37f44357aa6368338d7266cb7
wccou <1281585844@qq.com>
2018-07-12 22:01:22 +0800

    加一个标志位，当某一趟冒泡排序没有元素交换时，则冒泡结束，元素已经有序，可以有效的减少冒泡次数。

--- Sort/src/me/hitwcc/sort/BubbleSort.java
+++ Sort/src/me/hitwcc/sort/BubbleSort.java
@@ -36,10 +36,40 @@ public class BubbleSort {
 		return target;
 		// TODO Auto-generated constructor stub
 	}
-	
+	/**
+	 * 
+	 * @description:优化的冒泡 加一个标志位，当某一趟冒泡排序没有元素交换时，则冒泡结束，元素已经有序，可以有效的减少冒泡次数。
+	 * @author: wcc
+	 * @created: 下午9:59:44
+	 * @param target
+	 * @return
+	 */
+	public static int[] bubbleSortMy(int[] target) {
+		if(target==null||target.length ==0)
+			return null;
+		int n=target.length;
+		if(n==1){
+			return target;
+		}else {
+			boolean flag=true;
+			for(int i=0;i<n-1&&flag;i++) {
+				flag=false;
+				for(int j=n-1;j>i;j--) {
+					if(target[j]<target[j-1]) {
+						int temp = target[j];
+						target[j] = target[j-1];
+						target[j-1] = temp;
+						flag=true;
+					}
+				}
+			}
+			System.out.println(Arrays.toString(target));
+		}
+		return target;
+	}
 	public static void main(String[] args) {
-		int[] a = {9,8,7,6,5,10,11};
-		BubbleSort.bubbleSort(a);
+		int[] a = {9,8,7,6,5,10,11,4};
+		BubbleSort.bubbleSortMy(a);
 	}
 
 }
